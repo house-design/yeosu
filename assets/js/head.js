ed47c6944cca7494cc295aa053c24aeaa0af5e5a
seotory <seotory437@gmail.com>
2020-05-26 22:48:47 +0900

    코드 수정

--- assets/js/head.js
+++ assets/js/head.js
@@ -2,7 +2,7 @@
 (function(){
   var accessPossibleHost = ['localhost:4000', '127.0.0.1:4000', 'house-design.github.io', '여수인테리어.kr'];
   if (!accessPossibleHost.includes(location.host)) {
-    alert('불법복제된 사이트입니다.');
+    alert('코드불법 사용이 확인되었습니다.');
     window.onload = function () {
       document.open();
       document.write('close site');
